47e94ff95fa796f491e91336fa397d677ebdbb5a
HARSH SHUKLA <HARSHSHUKLA2022@VITBHOPAL.AC.IN>
2024-04-15 22:50:42 +0530

    updated

---
+++ leetcode/SumRootToLeafe.cpp
@@ -0,0 +1,18 @@
+class Solution
+{
+public:
+    vector<int> countElements(vector<int> &a, vector<int> &b, int n, vector<int> &query,
+                              int q)
+    {
+        sort(b.begin(), b.end());
+        vector<int> ans;
+
+        for (int i : query)
+        {
+            int index = upper_bound(b.begin(), b.end(), a[i]) - b.begin();
+            ans.push_back(index);
+        }
+
+        return ans;
+    }
+};
