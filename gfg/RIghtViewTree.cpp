2f8616bc703cbb35edc3ae494d64509bc4f16607
HARSH SHUKLA <HARSHSHUKLA2022@VITBHOPAL.AC.IN>
2024-04-26 18:33:15 +0530

    updated

---
+++ gfg/RIghtViewTree.cpp
@@ -0,0 +1,27 @@
+class Solution
+{
+public:
+    // Function to return list containing elements of right view of binary tree.
+
+    void solve(Node *root, vector<int> &ans, int level)
+    {
+        if (root == NULL)
+            return;
+
+        // we entered into the new level
+        if (level == ans.size())
+        {
+            ans.push_back(root->data);
+        }
+
+        solve(root->right, ans, level + 1);
+        solve(root->left, ans, level + 1);
+    }
+
+    vector<int> rightView(Node *root)
+    {
+        vector<int> ans;
+        solve(root, ans, 0);
+        return ans;
+    }
+};
